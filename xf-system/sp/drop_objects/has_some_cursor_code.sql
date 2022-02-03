60389b0150305c171cd191cefdfd37fdc569c7c9
smallGod <davies.mugume@gmail.com>
2022-02-03 22:23:37 +0300

    OHRI-523 Add view and table drop scripts

---
+++ xf-system/sp/drop_objects/has_some_cursor_code.sql
@@ -0,0 +1,47 @@
+USE dbo;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_xf_system_drop_all_views_in_schema;
+
+CREATE PROCEDURE sp_xf_system_drop_all_views_in_schema(
+    IN database_name NVARCHAR(255)
+)
+BEGIN
+
+    DECLARE view_name NVARCHAR(255);
+    DECLARE done INT DEFAULT FALSE;
+
+    DECLARE cursor_etl_views CURSOR FOR SELECT table_name FROM etl_views;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    SET @view_names := NULL;
+    SET @drop_view = '';
+
+    DROP TEMPORARY TABLE IF EXISTS etl_views;
+    CREATE TEMPORARY TABLE IF NOT EXISTS etl_views
+    SELECT table_name FROM information_schema.tables WHERE table_type = 'VIEW' AND table_schema = 'analysis';
+
+    #     SET @fill_etl_views := CONCAT('SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema='analysis'';);
+#     PREPARE fill_etl_views FROM @fill_etl_views;
+#     EXECUTE fill_etl_views;
+#     DEALLOCATE PREPARE fill_etl_views;
+
+    OPEN cursor_etl_views;
+    REPEAT
+        FETCH cursor_etl_views INTO view_name;
+
+        SET @drop_view = CONCAT(@drop_view, ' DROP VIEW IF EXISTS `', database_name, '`.`', view_name, '`;');
+
+    UNTIL done END REPEAT;
+    CLOSE cursor_etl_views;
+
+    PREPARE drop_view FROM @drop_view;
+    EXECUTE drop_view;
+    DEALLOCATE PREPARE drop_view;
+
+END//
+
+DELIMITER ;
+
+CALL sp_xf_system_drop_all_views_in_schema('analysis');
