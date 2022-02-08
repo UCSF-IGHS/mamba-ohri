7aeabeeb4a4d68e61dad3611a5fcfbff5488892c
smallGod <davies.mugume@gmail.com>
2022-02-08 09:24:42 +0300

    OHRI-523 Add dim_client scripts

--- ohri/base/sp_mamba_data_processing.sql
+++ ohri/base/sp_mamba_data_processing.sql
@@ -3,6 +3,7 @@ USE analysis;
 -- $BEGIN
 
 -- SELECT 'Starting execution of Base Objects';
+CALL dbo.sp_xf_system_drop_all_objects_in_schema('analysis');
 
 -- Base dimensions
 -- SELECT 'Executing sp_mamba_dim_concept_datatype';
@@ -26,7 +27,18 @@ CALL sp_mamba_dim_encounter;
 -- SELECT 'Executing sp_mamba_dim_concept_metadata';
 CALL sp_mamba_dim_concept_metadata;
 
--- Base Objects
+-- SELECT 'Executing sp_mamba_dim_client';
+CALL sp_mamba_dim_person;
+
+-- SELECT 'Executing sp_mamba_dim_client';
+CALL sp_mamba_dim_person_name;
+
+-- SELECT 'Executing sp_mamba_dim_client';
+CALL sp_mamba_dim_person_address;
+
+-- SELECT 'Executing sp_dim_client';
+CALL sp_dim_client;
+
 -- SELECT 'Executing sp_mamba_z_tables';
 CALL sp_mamba_z_tables;
 
