4a714f5a1e2a497f7bc95fd7589d0f5cf9ced6cf
smallGod <davies.mugume@gmail.com>
2022-02-23 11:19:10 +0300

    Add more columns to hts_fact table

--- xf-system/sp/drop_objects/sp_xf_system_drop_all_functions_in_schema.sql
+++ xf-system/sp/drop_objects/sp_xf_system_drop_all_functions_in_schema.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS dbo.sp_xf_system_drop_all_stored_functions_in_schema;
+DROP PROCEDURE IF EXISTS dbo.sp_xf_system_drop_all_stored_functions_in_schema//
 
 CREATE PROCEDURE dbo.sp_xf_system_drop_all_stored_functions_in_schema(
     IN database_name NVARCHAR(255)
