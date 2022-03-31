8dba1390610a14d3ed4a182c8913d60a1a89301d
smallGod <davies.mugume@gmail.com>
2022-03-31 20:00:35 +0300

    OHRI-773 Refactor multiselect value update code

--- ohri/base/facts/fact_hts_encounter/sp_fact_hts_encounter.sql
+++ ohri/base/facts/fact_hts_encounter/sp_fact_hts_encounter.sql
@@ -3,6 +3,6 @@ USE analysis;
 -- $BEGIN
 
 CALL sp_flatten_encounter('fact_hts', 'encounter_hts', '79c1f50f-f77d-42e2-ad2a-d29304dde2fe');
-CALL sp_fact_hts_cleaning();
+CALL sp_fact_hts_encounter_cleaning();
 
 -- $END
