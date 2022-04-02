c8da8ea422cb5b27c81aeb259e7d68c25c509ea2
smallGod <davies.mugume@gmail.com>
2022-04-02 18:55:13 +0300

    OHRI-773 add script to invoke fact-covid create/insert/clean SPs

---
+++ ohri/base/facts/fact_covid_encounter/sp_fact_covid_encounter_create.sql
@@ -0,0 +1,5 @@
+USE analysis;
+
+-- $BEGIN
+CALL sp_flat_encounter_table_create("encounter_covid");
+-- $END
--- ohri/base/facts/fact_hts_encounter/sp_fact_hts_encounter.sql
+++ ohri/base/facts/fact_hts_encounter/sp_fact_hts_encounter.sql
@@ -2,7 +2,6 @@ USE analysis;
 
 -- $BEGIN
 
-CALL sp_flatten_encounter('fact_hts', 'encounter_hts', '79c1f50f-f77d-42e2-ad2a-d29304dde2fe');
-CALL sp_base_fact_hts_encounter_cleaning();
+
 
 -- $END
