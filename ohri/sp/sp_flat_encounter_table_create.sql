7f7dbe02d08810ab876e632d42c7851295053af7
smallGod <davies.mugume@gmail.com>
2022-04-02 18:48:19 +0300

    OHRI-773 Refactor flat encounter table create and insert scripts

--- ohri/sp/sp_flat_encounter_table_create.sql
+++ ohri/sp/sp_flat_encounter_table_create.sql
@@ -3,26 +3,24 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS sp_flat_encounter_table_create;
 
 CREATE PROCEDURE sp_flat_encounter_table_create(
-    IN report NVARCHAR(255),
-    IN output_table_name NVARCHAR(255)
+    IN flat_encounter_table_name NVARCHAR(255)
 )
 BEGIN
 
     SET session group_concat_max_len = 20000;
     SET @sql := NULL;
-    SET @report_nm = report;
 
-    SET @drop_table = CONCAT('DROP TABLE IF EXISTS `', output_table_name, '`');
+    SET @drop_table = CONCAT('DROP TABLE IF EXISTS `', flat_encounter_table_name, '`');
 
     SELECT
       GROUP_CONCAT(DISTINCT
         CONCAT(' MAX(CASE WHEN column_label = ''', column_label, ''' THEN ', fn_get_obs_value_column(concept_datatype), ' END) ', column_label)
       ) INTO @sql
     FROM mamba_dim_concept_metadata
-        WHERE report_name = @report_nm;
+        WHERE flat_table_name = flat_encounter_table_name;
 
     SET @create_table = CONCAT(
-            'CREATE TABLE `', output_table_name ,'` SELECT encounter_id, person_id AS client_id, ', @sql, '
+            'CREATE TABLE `', flat_encounter_table_name ,'` SELECT encounter_id, person_id AS client_id, ', @sql, '
             FROM mamba_z_encounter_obs limit 0;');
 
     PREPARE deletetb FROM @drop_table;
--- ohri/sp/sp_flat_encounter_table_insert.sql
+++ ohri/sp/sp_flat_encounter_table_insert.sql
@@ -3,9 +3,7 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS sp_flat_encounter_table_insert;
 
 CREATE PROCEDURE sp_flat_encounter_table_insert(
-    IN report NVARCHAR(255),
-    IN output_table_name NVARCHAR(255),
-    IN encounter_type_uuid NVARCHAR(255)
+    IN flat_encounter_table_name NVARCHAR(255)
 )
 BEGIN
 
@@ -13,18 +11,18 @@ BEGIN
 
     SET @column_labels = (SELECT GROUP_CONCAT(COLUMN_NAME SEPARATOR ', ')
                           FROM INFORMATION_SCHEMA.COLUMNS
-                          WHERE TABLE_NAME = output_table_name
+                          WHERE TABLE_NAME = flat_encounter_table_name
                             AND TABLE_SCHEMA = Database());
 
-    TRUNCATE TABLE output_table_name;
+    TRUNCATE TABLE flat_encounter_table_name;
 
     SET @create_table = CONCAT(
-            'INSERT INTO `', output_table_name, '` SELECT ', @column_labels, '
+            'INSERT INTO `', flat_encounter_table_name, '` SELECT ', @column_labels, '
             FROM mamba_z_encounter_obs eo
                 INNER JOIN mamba_dim_concept_metadata cm
                 ON IF(cm.concept_answer_obs=1, cm.concept_uuid=eo.obs_value_coded_uuid, cm.concept_uuid=eo.obs_question_uuid)
-            WHERE eo.encounter_type_uuid = ''', encounter_type_uuid, '''
-            AND cm.report_name = ''', report, '''
+            WHERE cm.flat_table_name = ''', flat_encounter_table_name, '''
+            AND eo.encounter_type_uuid =  cm.encounter_type_uuid
             GROUP BY eo.encounter_id;');
 
     PREPARE createtb FROM @create_table;
